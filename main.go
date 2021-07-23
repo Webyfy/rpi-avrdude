525095d24cead1615de7f06df05c1749152a8019
dhan <dhanesh@webyfy.com>
2021-07-23 15:45:19 +0530

    implement gpio package

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"time"
 
+	"git.reach-iot.com/iot-master/rpi-avrdude/gpio"
 	"github.com/kataras/golog"
 )
 
 const (
 	OriginalFile = "avrdude-original"
+	resetPin     = 23
 )
 
 func main() {
@@ -28,6 +32,20 @@ func main() {
 	ownDir := getOwnDir()
 	originalExec := filepath.Join(ownDir, OriginalFile)
 	golog.Info(originalExec)
+
+	// Blink
+	pin := gpio.NewDigitalPin(resetPin)
+	if err := pin.Export(); err != nil {
+		log.Fatal(err)
+	}
+	defer pin.Unexport()
+	pin.Direction(gpio.OUT)
+	for i := 0; i < 30; i++ {
+		pin.Write(gpio.HIGH)
+		time.Sleep(time.Second)
+		pin.Write(gpio.LOW)
+		time.Sleep(time.Second)
+	}
 
 }
 
